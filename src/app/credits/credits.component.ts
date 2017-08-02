d8c592cde68fdf885d3c960b6baf1d2539b1e35e
Mohammad Khatib <mkhatib727@gmail.com>
2017-08-01 17:55:02 -0700

    Fix Crisp installation

--- src/app/credits/credits.component.ts
+++ src/app/credits/credits.component.ts
@@ -1,6 +1,10 @@
 import {
   Component,
-  OnInit
+  OnInit,
+  ElementRef,
+  AfterViewInit,
+  Renderer,
+  OnDestroy
 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
@@ -9,4 +13,27 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./credits.component.css'],
   templateUrl: './credits.component.html'
 })
-export class CreditsComponent {}
+export class CreditsComponent implements AfterViewInit, OnDestroy {
+  constructor(private elementRef: ElementRef,
+              private renderer: Renderer) {}
+
+  public ngOnDestroy() {
+    const crispClientEl = this.renderer.selectRootElement('.crisp-client');
+    this.renderer.invokeElementMethod(
+        window.document.body,
+      'removeChild', [crispClientEl]);
+  }
+
+  public ngAfterViewInit() {
+    window['$crisp'] = [];
+    window['CRISP_WEBSITE_ID'] = 'd82dda61-3eba-4268-a3b5-e5b24908ad45';
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = 'https://client.crisp.chat/l.js';
+    this.renderer.invokeElementMethod(
+        this.elementRef.nativeElement,
+      'appendChild', [script]);
+  }
+
+}
